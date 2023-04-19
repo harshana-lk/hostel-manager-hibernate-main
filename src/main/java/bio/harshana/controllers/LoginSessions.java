15ca978552951b4c571dc2e7652311064d7c0a8b
harshana-lk <harshanajayaweerayt@gmail.com>
2023-04-20 02:26:38 +0530

    Redesigned the Landing Page UI

---
+++ src/main/java/bio/harshana/controllers/LoginSessions.java
@@ -0,0 +1,7 @@
+package bio.harshana.controllers;
+
+import bio.harshana.dto.UserDTO;
+
+public class LoginSessions {
+    public static UserDTO user;
+}
---
+++ src/main/java/bio/harshana/util/TextFields.java
@@ -0,0 +1,6 @@
+package bio.harshana.util;
+
+public enum TextFields {
+    ID, NAME, LANKAN_ID, EMAIL, ADDRESS, PHONE, DOUBLE, INTEGER, NONE_CHARACTER, INVOICE, ROOM_ID, STRING
+
+}
